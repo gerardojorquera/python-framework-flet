ba8c219086e1f290f247a42545301b5e420cfc95
Gerardo Jorquera <gjorquera@medicenter.cl>
2026-06-30 21:40:40 -0400

    Commit inicial

---
+++ app.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+async def main(page: ft.Page):
+    page.title = "Flet (GUI)"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txtName = ft.TextField(label="Ingrese su nombre:", width=200)
+    lblGreeting = ft.Text()
+
+    def greet(e):
+        lblGreeting.value = f"Hola, {txtName.value}!"
+        page.update()
+
+    page.add(
+        txtName,
+        ft.ElevatedButton("Saludar!", on_click=greet),
+        lblGreeting
+    )
+
+if __name__ == "__main__":    
+    ft.app(target=main)
